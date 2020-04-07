8afa643282b6903c9c6df701e8acdd1595bd2882
anrom7 <anrom7@gmail.com>
2020-04-07 10:54:14 +0300

    Update examples.py
    fix import

--- examples.py
+++ examples.py
@@ -1,15 +1,15 @@
 # Fill a 1-D array with random values, then print the values.
-from arrays import Array
 import random
 import sys
 import array
+from arrays import Array
+
 # The constructor is called to create the array.
 value_list = Array(1)
 print('size of array = ', sys.getsizeof(value_list))
 compact_array = array.array('u',['a'])
 print('size of compact array = ', sys.getsizeof(compact_array))
 print('size of list = ',sys.getsizeof(['a']))
-
 
 # Fill the array with random floating-point values.
 for i in range(len(value_list)) :
@@ -19,9 +19,8 @@ for i in range(len(value_list)) :
     print(value_list[i])
 for i in value_list:
     print(i)
-"""
+
 # Count the number of occurrences of each letter in a text file.
-from arrays import Array
 # Create an array for the counters.
 counters = Array(127)
 counters.clear(0)
@@ -39,8 +38,7 @@ for i in range(26) :
     print( "{} - {:4d} {} - {:4d}".format(chr(65+i), counters[65+i],
                                       chr(97+i), counters[97+i]))
 
-"""
-
+# get size of list
 data = []
 for i in range(15):
 	a = len(data)
